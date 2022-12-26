1eb20f1b39b74dd0c01d57f1f0203f1c0129af1e
seog-jun <shong69@myseneca.ca>
2022-12-26 13:47:40 -0500

    Modified

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { query } = require("express-validator");
 require("dotenv").config();
-
+//
 const db = require("./database/models");
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/sample_mflix", {
